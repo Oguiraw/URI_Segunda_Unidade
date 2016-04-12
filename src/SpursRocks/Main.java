31e992ca417697ebddeab90cda3787d87eb1f4be
sergio.ewerton <sergio.ewerton@LAB5-DCX-11.dcx.ufpb.br>
2016-04-12 12:05:02 -0300

    Spurs Rocks completo

--- src/SpursRocks/Main.java
+++ src/SpursRocks/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		int quantTimes = ler.nextInt();
 		ler.nextLine();
 		int cont = 0;
+		String resultados = "";
 
 		while (quantTimes != 0) {
 			cont++;
@@ -24,7 +25,7 @@ public class Main {
 				if (posicao1 > posicao3) {
 					times[posicao0 - 1].addPontuacao(2);
 					times[posicao2 - 1].addPontuacao(1);
-				} else if (posicao1 < posicao3) {
+				} else {
 					times[posicao2 - 1].addPontuacao(2);
 					times[posicao0 - 1].addPontuacao(1);
 				}
@@ -35,65 +36,72 @@ public class Main {
 			}
 
 			ordenanDecrescente(times);
-			System.out.println("Instancia " + cont);
-			resultado(times);
-			System.out.println("\n");
+			resultados += ("Instancia " + cont) + ("\n" + resultado(times)) + "\n\n";
 			quantTimes = ler.nextInt();
 			ler.nextLine();
 		}
 		ler.close();
+		resultados = resultados.trim();
+		System.out.println(resultados);
 
 	}// Fim do main
-	
-	
-	public static void iniciasArrayTimes(Time[] times){
-		for(int i=0; i<times.length ; i++){
+
+	public static void iniciasArrayTimes(Time[] times) {
+		for (int i = 0; i < times.length; i++) {
 			times[i] = new Time(i + 1);
 		}
 	}
-	
-	public static void resultado(Time[] times){
-		String resultado = "";		
-		for(int x=0; x<times.length -1; x++){
-			if(times[x].pontuacao > times[x+1].pontuacao){
-				resultado += " " + times[x].numeroDoTime;
-			}else if(times[x].pontuacao < times[x+1].pontuacao){
-				resultado += " " + times[x+1].numeroDoTime;
-				Time aux = times[x];
-				times[x] = times[x+1];
-				times[x+1] = aux;
-			}else{
-				if((times[x].pontosMarcados / (times[x].pontosRecebidos + 0.0)) > (times[x+1].pontosMarcados / (times[x+1].pontosRecebidos + 0.0))){
-					resultado += " " + times[x].numeroDoTime;
-				}else if((times[x].pontosMarcados / (times[x].pontosRecebidos + 0.0)) < (times[x+1].pontosMarcados / (times[x+1].pontosRecebidos + 0.0))){
-					resultado += " " + times[x+1].numeroDoTime;
-					Time aux = times[x];
-					times[x] = times[x+1];
-					times[x+1] = aux;
-				}
-			}
-		}
-		resultado += " " + times[times.length -1].numeroDoTime;
-		System.out.println(resultado);
 
-	}
-	
-	public static void ordenanDecrescente(Time[] times){
+	public static void ordenanDecrescente(Time[] times) {
 		boolean teste = true;
-		while(teste){
+		while (teste) {
 			teste = false;
-			for(int x=0; x<times.length -1; x++){
-				if(times[x].pontuacao < times[x+1].pontuacao){
+			for (int x = 0; x < times.length - 1; x++) {
+				if (times[x].pontuacao < times[x + 1].pontuacao) {
 					Time aux = times[x];
-					times[x] = times[x+1];
-					times[x+1] = aux;
+					times[x] = times[x + 1];
+					times[x + 1] = aux;
 					teste = true;
+				} else if (times[x].pontuacao == times[x + 1].pontuacao) {
+					if ((times[x].pontosMarcados / (times[x].pontosRecebidos + 0.0)) < (times[x + 1].pontosMarcados
+							/ (times[x + 1].pontosRecebidos + 0.0))) {
+						Time aux = times[x];
+						times[x] = times[x + 1];
+						times[x + 1] = aux;
+						teste = true;
+					} else if ((times[x].pontosMarcados
+							/ (times[x].pontosRecebidos + 0.0)) == (times[x + 1].pontosMarcados
+									/ times[x + 1].pontosRecebidos + 0.0)) {
+
+						if (times[x].pontosMarcados < times[x + 1].pontosMarcados) {
+							Time aux = times[x];
+							times[x] = times[x + 1];
+							times[x + 1] = aux;
+							teste = true;
+						} else if (times[x].pontuacao == times[x + 1].pontuacao) {
+							if (times[x].numeroDoTime > times[x + 1].numeroDoTime) {
+								Time aux = times[x];
+								times[x] = times[x + 1];
+								times[x + 1] = aux;
+								teste = true;
+
+							}
+						}
+					}
 				}
 			}
 		}
-	}//Fim do Ordenar em ordem Decrescente
+	}
 
-	
+	public static String resultado(Time[] times) {
+		String resultado = "";
+		for (Time t : times) {
+			resultado += t.numeroDoTime + " ";
+		}
+		resultado = resultado.trim();
+		return resultado; 
+	}// Fim do resultado
+
 	public static class Time {
 		private int numeroDoTime;
 		private int pontuacao;
