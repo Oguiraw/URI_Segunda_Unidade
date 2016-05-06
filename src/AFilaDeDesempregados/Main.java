366bf79e017c5a3e5454661ebcdbdbf5f678e37a
Oguiraw <sergio.ewerton@dce.ufpb.br>
2016-05-05 22:17:18 -0300

    quase...

--- src/AFilaDeDesempregados/Main.java
+++ src/AFilaDeDesempregados/Main.java
@@ -3,21 +3,30 @@ package AFilaDeDesempregados;
 import java.util.Scanner;
 
 public class Main {
-
+	public static String resultado = "";
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
+		String entradaNext = ler.nextLine();
+		String[] entrada;
+		
 
-		String[] entrada = ler.nextLine().split(" ");
-		int quantDesempregados = Integer.parseInt(entrada[0]);
-		int pulosA = Integer.parseInt(entrada[1]);
-		int pulosB = Integer.parseInt(entrada[2]);
+		while (!(entradaNext.equals("0 0 0"))) {
+			entrada = entradaNext.split(" ");
+			int quantDesempregados = Integer.parseInt(entrada[0]);
+			int pulosA = Integer.parseInt(entrada[1]);
+			int pulosB = Integer.parseInt(entrada[2]);
+			int[] lista = new int[quantDesempregados];
+			for (int i = 0; i < quantDesempregados; i++) {
+				lista[i] = i;
+			}
+			resultado = "";
 
-		int[] lista = new int[quantDesempregados];
-		for (int i = 0; i < quantDesempregados; i++) {
-			lista[i] = i + 1;
+			escolherDesempregado(lista, pulosA, pulosB);
+			resultado = resultado.substring(0, resultado.length()-1);
+			System.out.println(resultado);
+			entradaNext = ler.nextLine();
+			
 		}
-
-		escolherDesempregado(lista, pulosA, pulosB);
 
 	}
 
@@ -30,26 +39,26 @@ public class Main {
 			int cont = puloSelecionadoA;
 
 			while (contPulos != pulosA) {
-				if (lista[cont] != 0) {
+				if (lista[cont] >= 0) {
 					contPulos++;
 				}
 
 				if (contPulos == pulosA) {
 					puloSelecionadoA = lista[cont];
-					break;					
+					break;
 				}
 
 				if (cont == (lista.length - 1)) {
 					cont = 0;
-				}else{
-					cont ++;
+				} else {
+					cont++;
 				}
 			}
-			
+
 			contPulos = 0;
 			cont = PuloselecionadoB;
 			while (contPulos != pulosB) {
-				if (lista[cont] != 0) {
+				if (lista[cont] >= 0) {
 					contPulos++;
 				}
 
@@ -59,21 +68,19 @@ public class Main {
 				}
 
 				if (cont == 0) {
-					cont = lista.length -1;
-				}else{
-					cont --;
+					cont = lista.length - 1;
+				} else {
+					cont--;
 				}
 			}
 
-
 			if (puloSelecionadoA == PuloselecionadoB) {
-				lista[puloSelecionadoA] = 0;
-				System.out.println(puloSelecionadoA + 1);
+				lista[puloSelecionadoA] = -1;
+				resultado += " " + (preparaString(puloSelecionadoA +1)) + ",";
 			} else {
-				lista[puloSelecionadoA] = 0;
-				System.out.println(puloSelecionadoA + 1);
-				lista[PuloselecionadoB] = 0;
-				System.out.println(PuloselecionadoB - 1);
+				lista[puloSelecionadoA] = -1;
+				lista[PuloselecionadoB] = -1;
+				resultado += " " + (preparaString(puloSelecionadoA +1)) + (preparaString(PuloselecionadoB +1)) + ",";
 			}
 		}
 
@@ -81,10 +88,21 @@ public class Main {
 
 	public static int quantPessoas(int[] lista) {
 		for (int i : lista) {
-			if (i != 0) {
+			if (i != -1) {
 				return 1;
 			}
 		}
 		return 0;
+	}
+	
+	public static String preparaString(int entrada){
+		String s = entrada + "";
+		if(s.length() == 1){
+			return "  " + s;
+		}else if(s.length() == 2){
+			return " " + s;
+		}else{
+			return s;
+		}
 	}
 }
